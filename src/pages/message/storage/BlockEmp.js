848719ba0a90e6d064a577a6348f2b7c40225af2
JH5256 <skdltmvhs2@naver.com>
2024-07-05 09:36:19 +0900

    commit update

---
+++ src/pages/message/storage/BlockEmp.js
@@ -0,0 +1,78 @@
+// function BlockEmp() {
+
+//     return (
+//         <div class="ly_cont">
+//             <h4 class="el_lv1Head hp_mb30">차단 관리</h4>
+//             <section class="bl_sect">
+//                 <table class="bl_tb2">
+//                     <colgroup>
+//                         <col style="width:200px" />
+//                         <col style="width:*" />
+//                         <col style="width:*" />
+//                         <col style="width:*" />
+//                     </colgroup>
+//                     <thead>
+//                         <tr>
+//                             <th scope="row">분류</th>
+//                             <th scope="row">차단 회원</th>
+//                             <th scope="row">차단 일자</th>
+//                             <th scope="row">관리</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody class="hp_alignC">
+//                         <tr>
+//                             <th scope="row" rowspan="3">차단 회원 관리<br />
+//                                 <button type="button" class="el_btnS el_btn8Bord hp_mt5">+ 추가</button>
+//                             </th>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="text" class="hp_w100" value="이재현" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="date" class="hp_w100" value="2024-05-17" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <button type="button" class="el_btnS el_btn8Back hp_ml5">차단 해제</button>
+//                             </td>
+//                         </tr>
+//                         <tr>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="text" class="hp_w100" value="이재현" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="date" class="hp_w100" value="2024-05-17" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <button type="button" class="el_btnS el_btn8Back hp_ml5">차단 해제</button>
+//                             </td>
+//                         </tr>
+//                         <tr>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="text" class="hp_w100" value="이재현" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <div class="ly_flex">
+//                                     <input type="date" class="hp_w100" value="2024-05-17" placeholder="보관함 이름 입력"/>
+//                                 </div>
+//                             </td>
+//                             <td>
+//                                 <button type="button" class="el_btnS el_btn8Back hp_ml5">차단 해제</button>
+//                             </td>
+//                         </tr>
+//                     </tbody>
+//                 </table>
+//             </section>
+//         </div>
+//     );
+// }
+
+// export default BlockEmp;
